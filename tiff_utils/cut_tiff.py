f086f6b8358f033a73a5b7cb4e864dae911c3cce
Sofiia Myntiuk <somynt@softserveinc.com>
2023-03-28 22:32:16 +0300

    update functions for tiff cutting and joining

--- tiff_utils/cut_tiff.py
+++ tiff_utils/cut_tiff.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import cv2
+import imutils
 import numpy as np
 import tifffile
 
@@ -16,7 +17,7 @@ def rotate(image):
     extended_image = cv2.copyMakeBorder(image, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=[0, 0, 0])
 
     # resize image to reduce noisy contours
-    scale_percent = 30
+    scale_percent = 100
     width = int(extended_image.shape[1] * scale_percent / 100)
     height = int(extended_image.shape[0] * scale_percent / 100)
     dim = (width, height)
@@ -54,37 +55,47 @@ def rotate(image):
     M, _ = cv2.estimateAffine2D(current_pts, dst_pts)
 
     output_shape = (int(sx / scale_percent * 100), int(sy / scale_percent * 100))
-    warped = cv2.warpAffine(image, M, output_shape)
+    warped = cv2.warpAffine(extended_image, M, (int(sx),int(sy)))
+    cv2.imwrite("thresh.png", threshed)
+    cv2.imwrite("thresh_warp.png", cv2.warpAffine(threshed, M, (int(sx),int(sy))))
     return warped, M, output_shape
 
 
-def minmax_stretch_one_channel(image):
+def minmax_stretch_one_channel(image, method="percentile"):
     """Min-max stretch for one-channel image."""
-    min_percent = 4
-    max_percent = 96
-    lo, hi = np.nanpercentile(image, (min_percent, max_percent))
+    if method == "percentile":
+        min_percent = 4
+        max_percent = 96
+        lo, hi = np.nanpercentile(image, (min_percent, max_percent))
+    elif method == "mean_std":
+        image_mean = np.mean(image)
+        image_std = np.std(image)
+        lo, hi = image_mean - 2 * image_std, image_mean + 2 * image_std
     res_img = (image.astype(float) - lo) / (hi - lo)
     return np.maximum(np.minimum(res_img * 255, 255), 0).astype(np.uint8)
 
 
-def minmax_stretch(image):
+def minmax_stretch(image, method="percentile"):
     """Min-max stretch for each channel of the image."""
-    new_image = []
-    i = 0
-    for channel in range(image.shape[2]):
-        channel_values = image[:, :, channel]
-        new_image.append((minmax_stretch_one_channel(channel_values)))
-        i += 1
-    new_image = np.dstack(new_image)
-    return new_image
+    if method is not None:
+        new_image = []
+        i = 0
+        for channel in range(image.shape[2]):
+            channel_values = image[:, :, channel]
+            new_image.append((minmax_stretch_one_channel(channel_values, method)))
+            i += 1
+        new_image = np.dstack(new_image)
+        return new_image
+    return image
 
 
-def cut_tiff_for_labeling(input_path, output_path, size=1024):
+def cut_tiff(input_path, output_path, size=1024, preprocessing_method="percentile"):
     """
     Cuts TIFF image into parts of the passed size.
     :param input_path: path to the input image.
     :param output_path: path to the output directory.
     :param size: size of the resulting tiles.
+    :preprocess: "percentile"/"mean_std"/None.
     :return: None.
     """
     filename = os.path.basename(input_path).split(".")[0]
@@ -94,14 +105,19 @@ def cut_tiff_for_labeling(input_path, output_path, size=1024):
         os.mkdir(f"{output_path}/{filename}_{size}")
 
     image = tifffile.imread(input_path)
+    if image.shape[0] <= 4:  # channels last
+        image = np.moveaxis(image, 0, -1)
     image = np.delete(image, [3], axis=2)[:, :, :]
-    image = image[0:image.shape[0] // 1024 * 1024, 0:image.shape[1] // 1024 * 1024, :]
     image = ((image - image.min()) * (1 / (image.max() - image.min()) * 255)).astype("uint8")
     image, _, _ = rotate(image)
-    image = minmax_stretch(image)
+    image = minmax_stretch(image, method=preprocessing_method)
+    tifffile.imwrite(os.path.join(input_path + "-rotated.tif"), image)
+    
+    range_x = range(0, image.shape[0] // size) if (image.shape[0] // size) else [0]
+    range_y = range(0, image.shape[1] // size) if (image.shape[0] // size) else [0]
 
-    for i in range(0, image.shape[0] // size):
-        for j in range(0, image.shape[1] // size):
+    for i in range_x:
+        for j in range_y:
             x = i * size
             y = j * size
             end_x = min(x + size, image.shape[0])
@@ -110,10 +126,10 @@ def cut_tiff_for_labeling(input_path, output_path, size=1024):
             end_y = image.shape[1] if image.shape[1] - end_y < size else end_y
             curr_part = image[x:end_x, y:end_y, :]
             if np.any(curr_part):
-                cv2.imwrite(f"./{output_path}/{filename}_{size}/{filename}_{x}_{y}.png", curr_part)
+                cv2.imwrite(f"{output_path}/{filename}_{size}/{filename}_{x}_{y}.png", curr_part)
 
 
-def cut_tiff_by_mask(input_path, mask_path, output_path, size=128):
+def cut_classify_tiff(input_path, mask_path, output_path, size=128, preprocess=True):
     """
     Cuts TIFF image and separated it into 'bombed' and 'not-bombed' directories.
     depending whether there are craters labeled on the mask.
@@ -135,12 +151,17 @@ def cut_tiff_by_mask(input_path, mask_path, output_path, size=128):
 
     image = tifffile.imread(input_path)
     image = np.delete(image, [3], axis=2)[:, :, :]
-    image = image[0:image.shape[0] // 1024 * 1024, 0:image.shape[1] // 1024 * 1024, :]
+    # image = image[0:image.shape[0] // 1024 * 1024, 0:image.shape[1] // 1024 * 1024, :]
     image = ((image - image.min()) * (1 / (image.max() - image.min()) * 255)).astype("uint8")
     image, M, output_shape = rotate(image)
+    if preprocess:
+        image = minmax_stretch(image)
 
+    if not mask_path:
+        mask_path = filename + "_mask.tif"
     mask = tifffile.imread(mask_path)
     mask = cv2.warpAffine(mask, M, output_shape)
+    cv2.imwrite("./mask.png", mask)
 
     for i in range(0, image.shape[0] // size):
         for j in range(0, image.shape[1] // size):
@@ -153,10 +174,9 @@ def cut_tiff_by_mask(input_path, mask_path, output_path, size=128):
             curr_part = image[x:end_x, y:end_y, :]
             mask_curr_part = mask[x:end_x, y:end_y]
             if np.any(curr_part):
-                # todo: filter by percentage of the mask
-                label = "bombed" if np.any(mask_curr_part) else "not-bombed"
-                cv2.imwrite(f"./{output_path}/{filename}_{size}/{label}/{filename}_{x}_{y}.png", curr_part)
-                # cv2.imwrite(f"./{output_path}/{filename}_{size}/{label}/{filename}_{x}_{y}_mask.png", mask_curr_part)
+                label = "bombed" if (np.count_nonzero(mask_curr_part) / mask_curr_part.size >= 0.03) else "not-bombed"
+                cv2.imwrite(f"{output_path}/{filename}_{size}/{label}/{filename}_{x}_{y}.png", curr_part)
+                cv2.imwrite(f"{output_path}/{filename}_{size}/{label}/{filename}_{x}_{y}_mask.png", mask_curr_part)
 
 
 if __name__ == "__main__":
@@ -170,5 +190,5 @@ if __name__ == "__main__":
     parser.add_argument("--size", "-s", type=int, default=1024,
                         help="Size of the output images.")
     args = parser.parse_args()
-
-    cut_tiff_by_mask(args.input, args.mask, args.output, args.size)
+    # cut_tiff(args.input, args.output, args.size, "percentile")
+    cut_classify_tiff(args.input, args.mask, args.output, args.size)
